0dd0255b5145df0fcba60911fbb9f7f046ecf2ae
Aristidina <aristidinaifa@gmail.com>
2025-06-20 12:32:12 +0100

    Mon app Flask Bonjour final

--- app1.py
+++ app1.py
@@ -28,5 +28,9 @@ def compte():
     return render_template('index.html')
 
 #lancer
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render définit cette variable automatiquement
+    app.run(host="0.0.0.0", port=port)
+
